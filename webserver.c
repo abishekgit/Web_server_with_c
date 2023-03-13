1f53eab4eb63ce86df1b07689b49a5619d734c3b
Somnath Golui <code.somu@gmail.com>
2023-03-13 13:34:13 +0530

    created the address to bind the socket to, and bind the socket to the address

--- webserver.c
+++ webserver.c
@@ -11,5 +11,20 @@ int main() {
 	}
 	printf("socket created successfully\n");
 
+	// Create the address to bind the socket to
+	struct sockaddr_in host_addr;
+	int host_addrlen = sizeof(host_addr);
+
+	host_addr.sin_family = AF_INET;
+	host_addr.sin_port = htons(PORT);
+	host_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	// Bind the socket to the address
+	if (bind(sockfd, (struct sockaddr *)&host_addr, host_addrlen) != 0) {
+		perror("webserver (bind)");
+		return 1;
+	}
+	printf("socket successfully bound to address\n");
+
 	return 0;
 }
