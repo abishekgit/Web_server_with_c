4c9dd256a7875fdeeec779f8bcc3d7b16ddd3a70
Abhishek Yadav <91972037+abishekgit@users.noreply.github.com>
2023-03-15 01:58:59 +0530

    Changes made
    Changes a couples of names too Like newsockfd --> client_socket

--- webserver.c
+++ webserver.c
@@ -1,6 +1,6 @@
 #include <arpa/inet.h>
+#include <errno.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -68,8 +68,8 @@ void serve_request(int client_socket) {
             "HTTP/1.0 200 OK\r\nServer: webserver-c\r\nContent-type: "
             "text/html\r\nContent-length: %ld\r\n\r\n%s",
             fsize, file_content);
-    ssize_t bytes_written = write(client_socket, response, strlen(response));
-    if (bytes_written == -1) {
+    ssize_t valwrite = write(client_socket, response, strlen(response));
+    if (valwrite == -1) {
       perror("Socket write failed");
     }
     close(client_socket);
@@ -82,8 +82,8 @@ void serve_request(int client_socket) {
       response,
       "HTTP/1.0 200 OK\r\nServer: webserver-c\r\nContent-type: "
       "text/html\r\n\r\n<html>This is a simple dynamic web page.</html>\r\n");
-  ssize_t bytes_written = write(client_socket, response, strlen(response));
-  if (bytes_written < 0) {
+  ssize_t valwrite = write(client_socket, response, strlen(response));
+  if (valwrite < 0) {
     perror("write");
     exit(EXIT_FAILURE);
   }
@@ -91,60 +91,75 @@ void serve_request(int client_socket) {
 }
 
 int main() {
-  // Create a socket
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd == -1) {
+    char buffer[BUFFER_SIZE];
+    char resp[] = "HTTP/1.0 200 OK\r\n"
+                  "Server: webserver-c\r\n"
+                  "Content-type: text/html\r\n\r\n"
+                  "<html>This a simple static webserver.</html>\r\n";
+
+    // Create a socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+      //editing belowe line to help with 
     perror("Socket creation failed");
-    exit(EXIT_FAILURE);
-  }
+    exit(EXIT_FAILURE)
+    }
+    printf("socket created successfully\n");
 
-  // Bind the socket to a specific address and port
-  struct sockaddr_in host_address;
-  host_address.sin_family = AF_INET;
-  host_address.sin_addr.s_addr = htonl(INADDR_ANY);
-  host_address.sin_port = htons(PORT);
+    // Create the address to bind the socket to
+    struct sockaddr_in host_addr;
+    int host_addrlen = sizeof(host_addr);
+  
+    host_addr.sin_family = AF_INET;
+    host_addr.sin_port = htons(PORT);
+    host_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-  if (bind(sockfd, (struct sockaddr *)&host_address, sizeof(host_address)) !=
-      0) {
+    // Create client address
+    struct sockaddr_in client_addr;
+    int client_addrlen = sizeof(client_addr);
+
+    // Bind the socket to the address
+    if (bind(sockfd, (struct sockaddr *)&host_addr, host_addrlen) != 0) {
     perror("Socket bind failed");
     exit(EXIT_FAILURE);
-  }
+    }
+    printf("socket successfully bound to address\n");
 
-  // Listen for incoming connections
-  if (listen(sockfd, SOMAXCONN) != 0) {
+    // Listen for incoming connections
+    if (listen(sockfd, SOMAXCONN) != 0) {
     perror("Socket listen failed");
     exit(EXIT_FAILURE);
-  }
-
-  printf("Server listening for connections on port %d...\n", PORT);
-
-  // Accept incoming connections
-  struct sockaddr_in client_address;
-  socklen_t addrlen = sizeof(client_address);
-  char buffer[BUFFER_SIZE];
-
-  for (;;) {
-    int client_socket =
-        accept(sockfd, (struct sockaddr *)&client_address, &addrlen);
-    if (client_socket == -1) {
-      perror("Socket accept failed");
-      continue;
     }
+    printf("server listening for connections\n");
 
-    printf("Connection accepted from %s:%d\n",
-           inet_ntoa(client_address.sin_addr), ntohs(client_address.sin_port));
+    for (;;) {
+        // Accept incoming connections
+        int client_socket = accept(sockfd, (struct sockaddr *)&client_addr,&client_addrlen);
+        if (client_socket < 0) {
+      perror("Socket accept failed");
+            continue;
+        }
+        printf("connection accepted\n");
 
-    // Read from the socket
-    int valread = read(client_socket, buffer, BUFFER_SIZE);
-    if (valread == -1) {
+        // Get client address 
+        int sockn = getsockname(client_socket, (struct sockaddr *)&client_addr,
+                                (socklen_t *)&client_addrlen);
+        if (sockn < 0) {
+            perror("webserver (getsockname)");
+            continue;
+        }
+
+        // Read from the socket
+        int valread = read(client_socket, buffer, BUFFER_SIZE);
+        if (valread < 0) {
       perror("Socket read failed");
       close(client_socket);
-      continue;
-    }
+            continue;
+        }
 
-    // Process the request
-    char method[BUFFER_SIZE], uri[BUFFER_SIZE], version[BUFFER_SIZE];
-    sscanf(buffer, "%s %s %s", method, uri, version);
+        // Read the request
+        char method[BUFFER_SIZE], uri[BUFFER_SIZE], version[BUFFER_SIZE];
+        sscanf(buffer, "%s %s %s", method, uri, version);
     printf("%s %s %s\n", method, version, uri);
 
     // Serve dynamic or static content depending on the URI
@@ -156,8 +171,9 @@ int main() {
       serve_static_content(client_socket, uri);
     }
 
-    close(client_socket);
-  }
 
-  return 0;
+        close(client_socket);
+    }
+
+    return 0;
 }
